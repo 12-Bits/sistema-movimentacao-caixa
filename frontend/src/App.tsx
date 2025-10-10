2d2da80c6c1c0381b906022b99edc6a18add16e0
Edmilson-11 <edmilsonsilva.ko@gmail.com>
2025-10-09 21:15:49 -0300

    Small ifxes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edita <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reat
         </a>
               
       </header>
